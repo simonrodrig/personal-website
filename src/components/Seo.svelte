c226a70e4f635bdcd17fa87780e7d58acd1abd7a
Simon Rodriguez <esimonrodrig@gmail.com>
2022-08-22 14:21:15 -0400

    seo additions

---
+++ src/components/Seo.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <meta name="description" content="Simon is a developer with an interest in frontend web development, UI/UX design, and open source.">
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Simon Rodriguez">
+  <meta name="twitter:description" content="Simon is a developer with an interest in frontend web development, UI/UX design, and open source.">
+  <meta name="twitter:image" content="https://www.rodrig.dev/twitter-card-image.png">
+  <meta name="twitter:image:alt" content="Image of Simon along with a short introduction">
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-  <title>Simon Rodriguez</title>
+  <title>Simon Rodriguez | Front-end Software Developer</title>
   <Seo />
 </svelte:head>
 
