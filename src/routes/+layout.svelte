f1de722379687572b815293634de67ffb5bc2280
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-19 23:06:10 -0500

    remove redundant SEO components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
   import Footer from "../components/navigation/Footer.svelte";
-  import NavBar from "../components/NavBar.svelte";
-  import Seo from "../components/Seo.svelte";
+  import NavBar from "../components/navigation/NavBar.svelte";
   import BlobBackground from "../components/Blobs/BlobBackground.svelte";
 
   // Load our icons onto the page
   import 'iconify-icon';
 </script>
 
-<svelte:head>
-  <title>Simon Rodriguez | Front-end Software Developer</title>
-  <Seo />
-</svelte:head>
-
 <BlobBackground />
-
 <NavBar />
 <slot />
 <Footer />
