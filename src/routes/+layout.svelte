02e0868e689dbbf39ae5d53bc5c4c99ec9d414f9
Simon Rodriguez <esimonrodrig@gmail.com>
2022-12-19 23:11:22 -0500

    revert directory name change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Footer from "../components/navigation/Footer.svelte";
   import NavBar from "../components/navigation/NavBar.svelte";
-  import BlobBackground from "../components/blobs/BlobBackground.svelte";
+  import BlobBackground from "../components/Blobs/BlobBackground.svelte";
 
   // Load our icons onto the page
   import 'iconify-icon';
